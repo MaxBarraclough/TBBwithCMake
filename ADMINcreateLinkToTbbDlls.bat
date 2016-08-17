9e3953e9c73101506dcfa5331bb39e655b2d6a4f
Max Barraclough <maxbarraclough@users.noreply.github.com>
2016-08-17 17:00:32 +0100

    Update and rename batch file
    Update to handle both DLLs.
    
    Renamed to emphasise must run as admin,
    and that it in fact handles two DLLs.

--- ADMINcreateLinkToTbbDlls.bat
+++ ADMINcreateLinkToTbbDlls.bat
@@ -7,3 +7,5 @@ REM Create a symlink rather than copying.
 REM Note that the args are in opposite order.
 mklink "%~dp0\tbb_debug.dll" "C:\Users\mb\Downloads\tbb44\tbb44_20160526oss\bin\intel64\vc14\tbb_debug.dll"
 
+mklink "%~dp0\tbb.dll" "C:\Users\mb\Downloads\tbb44\tbb44_20160526oss\bin\intel64\vc14\tbb.dll"
+
