67f6975d0d5eed90af8ca19d6e84b592bbc71598
Max Barraclough <maxbarraclough@users.noreply.github.com>
2016-08-18 15:34:13 +0100

    Overhaul small script to invoke CMake on Linux
    Specify location of TBB's Release .so file,
    which is required by our latest CMake file.
    Previously we'd handled only Debug builds.
    
    Also use wildcards for version-insensitivity.

--- configureCMakeLinux.sh
+++ configureCMakeLinux.sh
@@ -1,1 +1,23 @@
-cmake -G"Unix Makefiles" -DTBB_ROOT_DIR=/home/mb/Downloads/tbb44_20160526oss -DTBB_LIBRARIES=/home/mb/Downloads/tbb44_20160526oss/build/linux_intel64_gcc_cc4.8_libc2.19_kernel3.19.0_debug/  -DTBB_tbb_LIBRARY_DEBUG=/home/mb/Downloads/tbb44_20160526oss/build/linux_intel64_gcc_cc4.8_libc2.19_kernel3.19.0_debug/libtbb_debug.so
+#!/bin/bash
+
+## Dirty hard-coded BASH script to pass the correct flags to CMake when configuring on my system.
+## We use wildcards to (hopefully) be insensitive to TBB version.
+## Note that we *must* expand all paths before passing them to CMake
+
+## dirname command will expand path and give us the path of containing directory
+m="$(dirname /home/mb/Downloads/tbb*/README)" ## sanity check: ensure tbb dir contains README file
+
+## Use arrays to cause paths to be expanded.
+## Storing in a scalar variable won't have the same effect: path remains unexpanded.
+dArr=(/home/mb/Downloads/tbb*/build/linux_intel64_gcc*debug/libtbb_debug.so) ## array now holds expanded path
+d="${dArr[0]}"
+
+rArr=(/home/mb/Downloads/tbb*/build/linux_intel64*release/libtbb.so) ## array now holds expanded path
+r="${rArr[0]}"
+## the release .so file has no _release suffix
+
+cmake -G"Unix Makefiles"          \
+"-DTBB_ROOT_DIR=${m}"             \
+"-DTBB_tbb_LIBRARY_DEBUG=${d}"    \
+"-DTBB_tbb_LIBRARY_RELEASE=${r}"
+
